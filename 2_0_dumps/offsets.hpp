711a8a557e4eeba6680bdad6704d4e8d9e628d67
Sepeix <147322855+CBXPL@users.noreply.github.com>
2026-02-19 20:26:59 +0100

    first dumps with DD2 0.0.5

---
+++ 2_0_dumps/offsets.hpp
@@ -0,0 +1,25 @@
+// Dumper with DreamyDumper2
+// Dump date: 2026-02-19 20:22
+
+#pragma once
+
+namespace dreamydumper_offsets {
+    // dwEntityList
+    inline const char* dwEntityList_location = "0x14A7BE9";
+    inline const char* dwEntityList_sig = "48 89 0D ?? ?? ?? ?? E9 ?? ?? ?? ?? CC";
+    inline constexpr int dwEntityList_offset = 38445272;
+    inline const char* dwEntityList_hex = "0x24AA0D8";
+
+    // dwViewMatrix
+    inline const char* dwViewMatrix_location = "0xB7A393";
+    inline const char* dwViewMatrix_sig = "48 8D 0D ?? ?? ?? ?? 48 C1 E0 06";
+    inline constexpr int dwViewMatrix_offset = 36744672;
+    inline const char* dwViewMatrix_hex = "0x230ADE0";
+
+    // dwGlobalVars
+    inline const char* dwGlobalVars_location = "0xAF471C";
+    inline const char* dwGlobalVars_sig = "48 89 15 ?? ?? ?? ?? 48 89 42";
+    inline constexpr int dwGlobalVars_offset = 33920464;
+    inline const char* dwGlobalVars_hex = "0x20595D0";
+
+}
---
+++ 2_0_dumps/weaponbase.hpp
@@ -0,0 +1,43 @@
+// Dumper with DreamyDumper2
+// Dump date: 2026-02-19 20:22
+
+#pragma once
+
+namespace dreamydumper_weaponbase {
+    // m_flSpread Info
+    inline const char* m_flSpread_location = "0x7A0B82";
+    inline const char* m_flSpread_pattern = "C4 B9 11 0F 4C 8D 8F ?? ?? ?? ?? 48 89 45 F8";
+    inline constexpr int m_flSpread_offset = 1648;
+    inline const char* m_flSpread_hex = "0x670";
+
+    // m_nRecoilSeed Info
+    inline const char* m_nRecoilSeed_location = "0x7A104D";
+    inline const char* m_nRecoilSeed_pattern = "FB B4 11 BC 4C 8D 8F ?? ?? ?? ?? 48 89 45 F8";
+    inline constexpr int m_nRecoilSeed_offset = 1780;
+    inline const char* m_nRecoilSeed_hex = "0x6F4";
+
+    // m_nSpreadSeed Info
+    inline const char* m_nSpreadSeed_location = "0x7A107B";
+    inline const char* m_nSpreadSeed_pattern = "9B A1 31 36 48 89 45 F8 4C 8D 8F ?? ?? ?? ??";
+    inline constexpr int m_nSpreadSeed_offset = 1784;
+    inline const char* m_nSpreadSeed_hex = "0x6F8";
+
+    // m_flRecoilMagnitude Info
+    inline const char* m_flRecoilMagnitude_location = "0x7A0D77";
+    inline const char* m_flRecoilMagnitude_pattern = "A1 67 6A 85 4C 8D 8F ?? ?? ?? ?? 48 89 45 F8";
+    inline constexpr int m_flRecoilMagnitude_offset = 1728;
+    inline const char* m_flRecoilMagnitude_hex = "0x6C0";
+
+    // m_flInaccuracyMove Info
+    inline const char* m_flInaccuracyMove_location = "0x7A0CDD";
+    inline const char* m_flInaccuracyMove_pattern = "39 25 C9 17 4C 8D 8F ?? ?? ?? ?? 48 89 45 F8";
+    inline constexpr int m_flInaccuracyMove_offset = 1704;
+    inline const char* m_flInaccuracyMove_hex = "0x6A8";
+
+    // m_flRecoilAngle Info
+    inline const char* m_flRecoilAngle_location = "0x7A0D13";
+    inline const char* m_flRecoilAngle_pattern = "C4 AB 2B FA 4C 8D 8F ?? ?? ?? ?? 48 89 45 F8";
+    inline constexpr int m_flRecoilAngle_offset = 1712;
+    inline const char* m_flRecoilAngle_hex = "0x6B0";
+
+}
