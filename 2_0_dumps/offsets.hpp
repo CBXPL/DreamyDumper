c6e631eb91e1fc749e9165d19673c359816cada0
Sepeix <147322855+CBXPL@users.noreply.github.com>
2026-02-24 10:54:42 +0100

    DD2 Dumps 24.02.2026

--- 2_0_dumps/offsets.hpp
+++ 2_0_dumps/offsets.hpp
@@ -1,25 +1,25 @@
 // Dumper with DreamyDumper2
-// Dump date: 2026-02-19 20:22
+// Dump date: 2026-02-24 10:53
 
 #pragma once
 
 namespace dreamydumper_offsets {
     // dwEntityList
-    inline const char* dwEntityList_location = "0x14A7BE9";
+    inline const char* dwEntityList_location = "0x14A9339";
     inline const char* dwEntityList_sig = "48 89 0D ?? ?? ?? ?? E9 ?? ?? ?? ?? CC";
-    inline constexpr int dwEntityList_offset = 38445272;
-    inline const char* dwEntityList_hex = "0x24AA0D8";
+    inline constexpr int dwEntityList_offset = 38449608;
+    inline const char* dwEntityList_hex = "0x24AB1C8";
 
     // dwViewMatrix
-    inline const char* dwViewMatrix_location = "0xB7A393";
+    inline const char* dwViewMatrix_location = "0xB7BB33";
     inline const char* dwViewMatrix_sig = "48 8D 0D ?? ?? ?? ?? 48 C1 E0 06";
-    inline constexpr int dwViewMatrix_offset = 36744672;
-    inline const char* dwViewMatrix_hex = "0x230ADE0";
+    inline constexpr int dwViewMatrix_offset = 36749024;
+    inline const char* dwViewMatrix_hex = "0x230BEE0";
 
     // dwGlobalVars
-    inline const char* dwGlobalVars_location = "0xAF471C";
+    inline const char* dwGlobalVars_location = "0xAF5EBC";
     inline const char* dwGlobalVars_sig = "48 89 15 ?? ?? ?? ?? 48 89 42";
-    inline constexpr int dwGlobalVars_offset = 33920464;
-    inline const char* dwGlobalVars_hex = "0x20595D0";
+    inline constexpr int dwGlobalVars_offset = 33924480;
+    inline const char* dwGlobalVars_hex = "0x205A580";
 
 }
--- 2_0_dumps/player.hpp
+++ 2_0_dumps/player.hpp
@@ -1,53 +1,53 @@
 // Dumper with DreamyDumper2
-// Dump date: 2026-02-19 20:22
+// Dump date: 2026-02-24 10:53
 
 #pragma once
 
 namespace dreamydumper_player {
     // m_iHealth Info
-    inline const char* m_iHealth_location = "0x42CF02";
+    inline const char* m_iHealth_location = "0x42CF82";
     inline const char* m_iHealth_pattern = "41 08 C6 01 05 C7 41 10 ?? ?? ?? ?? 66 89 69";
     inline constexpr int m_iHealth_offset = 852;
     inline const char* m_iHealth_hex = "0x354";
 
     // m_iTeamNum Info
-    inline const char* m_iTeamNum_location = "0x42D516";
+    inline const char* m_iTeamNum_location = "0x42D596";
     inline const char* m_iTeamNum_pattern = "41 08 C6 01 39 C7 41 10 ?? ?? ?? ?? 66 89 69";
     inline constexpr int m_iTeamNum_offset = 1011;
     inline const char* m_iTeamNum_hex = "0x3F3";
 
     // m_hPawn Info
-    inline const char* m_hPawn_location = "0x89A7D2";
+    inline const char* m_hPawn_location = "0x89BF62";
     inline const char* m_hPawn_pattern = "41 08 C6 01 0D C7 41 10 ?? ?? ?? ?? 66 89 69";
     inline constexpr int m_hPawn_offset = 1732;
     inline const char* m_hPawn_hex = "0x6C4";
 
     // m_hPlayerPawn Info
-    inline const char* m_hPlayerPawn_location = "0x7FB177";
+    inline const char* m_hPlayerPawn_location = "0x7FC907";
     inline const char* m_hPlayerPawn_pattern = "41 08 C6 01 0D C7 41 10 ?? ?? ?? ?? 66 89 69";
     inline constexpr int m_hPlayerPawn_offset = 2316;
     inline const char* m_hPlayerPawn_hex = "0x90C";
 
     // m_lifeState Info
-    inline const char* m_lifeState_location = "0x42EC73";
-    inline const char* m_lifeState_pattern = "05 86 DB 53 01 C7 45 C7 ?? ?? ?? ?? 4C 8D 4D";
+    inline const char* m_lifeState_location = "0x42ECF3";
+    inline const char* m_lifeState_pattern = "05 AE FF 53 01 C7 45 C7 ?? ?? ?? ?? 4C 8D 4D";
     inline constexpr int m_lifeState_offset = 860;
     inline const char* m_lifeState_hex = "0x35C";
 
     // m_vecAbsVelocity Info
-    inline const char* m_vecAbsVelocity_location = "0x42D792";
+    inline const char* m_vecAbsVelocity_location = "0x42D812";
     inline const char* m_vecAbsVelocity_pattern = "41 08 C6 01 03 C7 41 10 ?? ?? ?? ?? 66 89 69";
     inline constexpr int m_vecAbsVelocity_offset = 1028;
     inline const char* m_vecAbsVelocity_hex = "0x404";
 
     // m_pGameSceneNode Info
-    inline const char* m_pGameSceneNode_location = "0x7FFC95E63450";
+    inline const char* m_pGameSceneNode_location = "0x7FF9DC0753D0";
     inline const char* m_pGameSceneNode_pattern = "STATIC_DATA_TABLE";
     inline constexpr int m_pGameSceneNode_offset = 824;
     inline const char* m_pGameSceneNode_hex = "0x338";
 
     // m_fFlags Info
-    inline const char* m_fFlags_location = "0x42D652";
+    inline const char* m_fFlags_location = "0x42D6D2";
     inline const char* m_fFlags_pattern = "41 08 C6 01 25 C7 41 10 ?? ?? ?? ?? 66 89 69";
     inline constexpr int m_fFlags_offset = 1024;
     inline const char* m_fFlags_hex = "0x400";
