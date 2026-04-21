3a099aa5387123cd0fef090e7ce746a0beafdede
Sepeix <147322855+CBXPL@users.noreply.github.com>
2026-04-21 15:15:05 +0200

    AnimGraph2 21.04.2026

--- 2_0_dumps/offsets.hpp
+++ 2_0_dumps/offsets.hpp
@@ -1,37 +1,37 @@
 // Dumper with DreamyDumper2
-// Dump date: 2026-04-19 20:14
+// Dump date: 2026-04-21 15:11
 
 #pragma once
 
 namespace dreamydumper_offsets {
     // dwEntityList
-    inline const char* dwEntityList_location = "0x14AFD09";
+    inline const char* dwEntityList_location = "0x14A65D9";
     inline const char* dwEntityList_sig = "48 89 0D ?? ?? ?? ?? E9 ?? ?? ?? ?? CC";
-    inline constexpr int dwEntityList_offset = 38482536;
-    inline const char* dwEntityList_hex = "0x24B3268";
+    inline constexpr int dwEntityList_offset = 38573840;
+    inline const char* dwEntityList_hex = "0x24C9710";
 
     // dwViewMatrix
-    inline const char* dwViewMatrix_location = "0xB7D5F3";
+    inline const char* dwViewMatrix_location = "0xB67863";
     inline const char* dwViewMatrix_sig = "48 8D 0D ?? ?? ?? ?? 48 C1 E0 06";
-    inline constexpr int dwViewMatrix_offset = 36781840;
-    inline const char* dwViewMatrix_hex = "0x2313F10";
+    inline constexpr int dwViewMatrix_offset = 36868304;
+    inline const char* dwViewMatrix_hex = "0x23290D0";
 
     // dwGlobalVars
-    inline const char* dwGlobalVars_location = "0xAF795C";
+    inline const char* dwGlobalVars_location = "0xADEADC";
     inline const char* dwGlobalVars_sig = "48 89 15 ?? ?? ?? ?? 48 89 42";
-    inline constexpr int dwGlobalVars_offset = 33957184;
-    inline const char* dwGlobalVars_hex = "0x2062540";
+    inline constexpr int dwGlobalVars_offset = 33834216;
+    inline const char* dwGlobalVars_hex = "0x20444E8";
 
     // dwLocalPlayerController
-    inline const char* dwLocalPlayerController_location = "0x8AEEA0";
+    inline const char* dwLocalPlayerController_location = "0x8A8AE0";
     inline const char* dwLocalPlayerController_sig = "48 8B 05 ?? ?? ?? ?? 41 89 BE";
-    inline constexpr int dwLocalPlayerController_offset = 36667432;
-    inline const char* dwLocalPlayerController_hex = "0x22F8028";
+    inline constexpr int dwLocalPlayerController_offset = 36712064;
+    inline const char* dwLocalPlayerController_hex = "0x2302E80";
 
     // dwViewRender
-    inline const char* dwViewRender_location = "0xAEA679";
+    inline const char* dwViewRender_location = "0xAD1489";
     inline const char* dwViewRender_sig = "48 89 05 ?? ?? ?? ?? 48 8B C8 48 85 C0";
-    inline constexpr int dwViewRender_offset = 36782888;
-    inline const char* dwViewRender_hex = "0x2314328";
+    inline constexpr int dwViewRender_offset = 36868840;
+    inline const char* dwViewRender_hex = "0x23292E8";
 
 }
