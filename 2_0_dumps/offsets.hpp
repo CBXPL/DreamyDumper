3b4a9f60ca4c3527e14034b1a8b1c6cfb62f0c44
Sepeix <147322855+CBXPL@users.noreply.github.com>
2026-05-09 18:21:17 +0200

    DD2 upd dumps 09.05.26

--- 2_0_dumps/offsets.hpp
+++ 2_0_dumps/offsets.hpp
@@ -1,37 +1,37 @@
 // Dumper with DreamyDumper2
-// Dump date: 2026-04-21 15:11
+// Dump date: 2026-05-09 18:19
 
 #pragma once
 
 namespace dreamydumper_offsets {
     // dwEntityList
-    inline const char* dwEntityList_location = "0x14A65D9";
+    inline const char* dwEntityList_location = "0x14ADC69";
     inline const char* dwEntityList_sig = "48 89 0D ?? ?? ?? ?? E9 ?? ?? ?? ?? CC";
-    inline constexpr int dwEntityList_offset = 38573840;
-    inline const char* dwEntityList_hex = "0x24C9710";
+    inline constexpr int dwEntityList_offset = 38604224;
+    inline const char* dwEntityList_hex = "0x24D0DC0";
 
     // dwViewMatrix
-    inline const char* dwViewMatrix_location = "0xB67863";
+    inline const char* dwViewMatrix_location = "0xB6D433";
     inline const char* dwViewMatrix_sig = "48 8D 0D ?? ?? ?? ?? 48 C1 E0 06";
-    inline constexpr int dwViewMatrix_offset = 36868304;
-    inline const char* dwViewMatrix_hex = "0x23290D0";
+    inline constexpr int dwViewMatrix_offset = 36899552;
+    inline const char* dwViewMatrix_hex = "0x2330AE0";
 
     // dwGlobalVars
-    inline const char* dwGlobalVars_location = "0xADEADC";
+    inline const char* dwGlobalVars_location = "0xAE458C";
     inline const char* dwGlobalVars_sig = "48 89 15 ?? ?? ?? ?? 48 89 42";
-    inline constexpr int dwGlobalVars_offset = 33834216;
-    inline const char* dwGlobalVars_hex = "0x20444E8";
+    inline constexpr int dwGlobalVars_offset = 33863128;
+    inline const char* dwGlobalVars_hex = "0x204B5D8";
 
     // dwLocalPlayerController
-    inline const char* dwLocalPlayerController_location = "0x8A8AE0";
+    inline const char* dwLocalPlayerController_location = "0x8AE520";
     inline const char* dwLocalPlayerController_sig = "48 8B 05 ?? ?? ?? ?? 41 89 BE";
-    inline constexpr int dwLocalPlayerController_offset = 36712064;
-    inline const char* dwLocalPlayerController_hex = "0x2302E80";
+    inline constexpr int dwLocalPlayerController_offset = 36742384;
+    inline const char* dwLocalPlayerController_hex = "0x230A4F0";
 
     // dwViewRender
-    inline const char* dwViewRender_location = "0xAD1489";
+    inline const char* dwViewRender_location = "0xAD6F39";
     inline const char* dwViewRender_sig = "48 89 05 ?? ?? ?? ?? 48 8B C8 48 85 C0";
-    inline constexpr int dwViewRender_offset = 36868840;
-    inline const char* dwViewRender_hex = "0x23292E8";
+    inline constexpr int dwViewRender_offset = 36895952;
+    inline const char* dwViewRender_hex = "0x232FCD0";
 
 }
--- 2_0_dumps/weaponbase.hpp
+++ 2_0_dumps/weaponbase.hpp
@@ -1,53 +1,53 @@
 // Dumper with DreamyDumper2
-// Dump date: 2026-04-21 15:11
+// Dump date: 2026-05-09 18:19
 
 #pragma once
 
 namespace dreamydumper_weaponbase {
     // m_flSpread Info
-    inline const char* m_flSpread_location = "0x7A15E6";
+    inline const char* m_flSpread_location = "0x7A1076";
     inline const char* m_flSpread_pattern = "C4 B9 11 0F 4C 8D 8F ?? ?? ?? ?? 48 89";
     inline constexpr int m_flSpread_offset = 1880;
     inline const char* m_flSpread_hex = "0x758";
 
     // m_nRecoilSeed Info
-    inline const char* m_nRecoilSeed_location = "0x7A1AB1";
+    inline const char* m_nRecoilSeed_location = "0x7A1541";
     inline const char* m_nRecoilSeed_pattern = "FB B4 11 BC 4C 8D 8F ?? ?? ?? ?? 48 89";
     inline constexpr int m_nRecoilSeed_offset = 2012;
     inline const char* m_nRecoilSeed_hex = "0x7DC";
 
     // m_nSpreadSeed Info
-    inline const char* m_nSpreadSeed_location = "0x7A1ADF";
+    inline const char* m_nSpreadSeed_location = "0x7A156F";
     inline const char* m_nSpreadSeed_pattern = "9B A1 31 36 48 89 45 F8 4C 8D 8F ?? ?? ?? ?? C7 45";
     inline constexpr int m_nSpreadSeed_offset = 2016;
     inline const char* m_nSpreadSeed_hex = "0x7E0";
 
     // m_flRecoilMagnitude Info
-    inline const char* m_flRecoilMagnitude_location = "0x7A17DB";
+    inline const char* m_flRecoilMagnitude_location = "0x7A126B";
     inline const char* m_flRecoilMagnitude_pattern = "A1 67 6A 85 4C 8D 8F ?? ?? ?? ?? 48 89";
     inline constexpr int m_flRecoilMagnitude_offset = 1960;
     inline const char* m_flRecoilMagnitude_hex = "0x7A8";
 
     // m_flInaccuracyMove Info
-    inline const char* m_flInaccuracyMove_location = "0x7A1741";
+    inline const char* m_flInaccuracyMove_location = "0x7A11D1";
     inline const char* m_flInaccuracyMove_pattern = "39 25 C9 17 4C 8D 8F ?? ?? ?? ?? 48 89";
     inline constexpr int m_flInaccuracyMove_offset = 1936;
     inline const char* m_flInaccuracyMove_hex = "0x790";
 
     // m_pWeaponServices Info
-    inline const char* m_pWeaponServices_location = "0x4029F6";
-    inline const char* m_pWeaponServices_pattern = "48 8D 05 E3 1B 56 01 48 03 DA 48 89 45 BF 43 8B 04 2E 39 05 AA B3 E5 01 C7 45 C7 ?? ?? ?? ?? 66 44";
-    inline constexpr int m_pWeaponServices_offset = 4576;
-    inline const char* m_pWeaponServices_hex = "0x11E0";
+    inline const char* m_pWeaponServices_location = "0x402B16";
+    inline const char* m_pWeaponServices_pattern = "48 8D 05 33 8B 56 ?? ?? ?? ?? 48 89 45";
+    inline constexpr int m_pWeaponServices_offset = 1;
+    inline const char* m_pWeaponServices_hex = "0x1";
 
     // m_hActiveWeapon Info
-    inline const char* m_hActiveWeapon_location = "0x9DFD26";
-    inline const char* m_hActiveWeapon_pattern = "48 8D 05 ? ? ? ? 48 89 41 08 C6 01 0D C7 41 10 ?? ?? ?? ??";
-    inline constexpr int m_hActiveWeapon_offset = 96;
-    inline const char* m_hActiveWeapon_hex = "0x60";
+    inline const char* m_hActiveWeapon_location = "0x30AA50";
+    inline const char* m_hActiveWeapon_pattern = "48 8D 05 89 64 ?? ?? ?? ?? 41 08 C6 01 0D C7 41 10 B0 0F 00 00";
+    inline constexpr int m_hActiveWeapon_offset = 100;
+    inline const char* m_hActiveWeapon_hex = "0x64";
 
     // m_hMyWeapons Info
-    inline const char* m_hMyWeapons_location = "0x2032D70";
+    inline const char* m_hMyWeapons_location = "0x2039EE0";
     inline const char* m_hMyWeapons_pattern = "STATIC_DATA_TABLE";
     inline constexpr int m_hMyWeapons_offset = 72;
     inline const char* m_hMyWeapons_hex = "0x48";
