2c55efc2ce52bc71231e640c0f3baa27febf69b8
Sepeix <147322855+CBXPL@users.noreply.github.com>
2026-05-23 12:02:00 +0200

    Jerr Kmeit 23.05.2026

--- dmp_off/steamaudio_dll.cs
+++ dmp_off/steamaudio_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-05-21 16:20:30.129663700 UTC
+// 2026-05-23 09:59:53.825703300 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: steamaudio.dll
