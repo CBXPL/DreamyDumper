43fdf22a64eb14c1dabb591156fd0f7035a72515
Sepeix <147322855+CBXPL@users.noreply.github.com>
2026-07-01 12:21:37 +0200

    bout_2 01072026

--- dmp_off/rendersystemdx11_dll.cs
+++ dmp_off/rendersystemdx11_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-06-15 16:04:01.519755300 UTC
+// 2026-07-01 10:20:43.132551200 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: rendersystemdx11.dll
